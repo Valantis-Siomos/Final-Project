5e13759b0f50b78ae7db2caefc657152e0ce2bd4
Valantis <siopis2008@hotmail.com>
2023-11-23 10:20:52 +0200

    carousel handleImageClick

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import category1 from "../assets/category1.jpeg";
 import category2 from "../assets/category2.jpg";
 import category3 from "../assets/category3.jpg";
 import category4 from "../assets/category4.jpg";
-import "./carousel.css"
+import "./carousel.css";
 
 const Carousel = (categories) => {
   const settings = {
@@ -19,53 +19,108 @@ const Carousel = (categories) => {
     autoplaySapeed: 2000,
   };
 
-  
+  const handleImageClick = (category) => {
+    window.location.href = `/category/${encodeURIComponent(category)}`;
+  };
 
   return (
     <Slider {...settings}>
-      <div className='carousel-image'>
-        <img className='imgcar' src={category1} alt="Category 1" style={{
-                  width: "1200px", 
-                  height: "630px", 
-                  objectFit: "cover",
-                //   marginLeft: "651px",
-                  marginTop: "50px",
-                  marginLeft: "351.5px",
-                }} />
-                <p style={{ marginTop: "-100px", color: "white", fontSize: "40px", fontWeight: 'bold' }}>BEDROOM</p>
+      <div className="carousel-image">
+        <img
+          className="imgcar"
+          src={category1}
+          alt="Category 1"
+          style={{
+            width: "1200px",
+            height: "630px",
+            objectFit: "cover",
+            marginTop: "50px",
+            marginLeft: "351.5px",
+          }}
+          onClick={() => handleImageClick("Bedroom")}
+        />
+        <p
+          style={{
+            marginTop: "-100px",
+            color: "white",
+            fontSize: "40px",
+            fontWeight: "bold",
+          }}
+        >
+          BEDROOM
+        </p>
       </div>
-      <div className='carousel-image'>
-        <img src={category2} alt="Category 2" style={{
-                   width: "1200px",
-                   height: "630px",
-                  objectFit: "cover",
-                //   marginLeft: "-10px",
-                marginTop: "50px",
-                marginLeft: "351.5px",
-                }}/>
-                <p style={{ marginTop: "-100px", color: "white", fontSize: "40px", fontWeight: 'bold' }}>DECORATION</p>
+      <div className="carousel-image">
+        <img
+          src={category2}
+          alt="Category 2"
+          style={{
+            width: "1200px",
+            height: "630px",
+            objectFit: "cover",
+            marginTop: "50px",
+            marginLeft: "351.5px",
+          }}
+          onClick={() => handleImageClick("Decoration")}
+        />
+        <p
+          style={{
+            marginTop: "-100px",
+            color: "white",
+            fontSize: "40px",
+            fontWeight: "bold",
+          }}
+        >
+          DECORATION
+        </p>
       </div>
-      <div className='carousel-image'>
-      <img src={category3} alt="Category 3" style={{
-                   width: "1200px",
-                   height: "600px",
-                  objectFit: "cover",
-                //   marginLeft: "-10px",
-                marginTop: "50px",
-                marginLeft: "351.5px",
-                }}/>
-                <p style={{ marginTop: "-100px", color: "white", fontSize: "40px", fontWeight: 'bold' }}>OFFICE</p>
+      <div className="carousel-image">
+        <img
+          src={category3}
+          alt="Category 3"
+          style={{
+            width: "1200px",
+            height: "600px",
+            objectFit: "cover",
+            marginTop: "50px",
+            marginLeft: "351.5px",
+          }}
+          onClick={() => handleImageClick("Office")}
+        />
+        <p
+          style={{
+            marginTop: "-100px",
+            color: "white",
+            fontSize: "40px",
+            fontWeight: "bold",
+          }}
+        >
+          OFFICE
+        </p>
       </div>
-      <div className='carousel-image'>
-      <img src={category4} alt="Category 4" style={{
-                   width: "1200px",
-                   height: "600px",
-                  objectFit: "cover",
-                //   marginLeft: "-10px",
-                marginTop: "50px",
-                marginLeft: "351.5px",
-                }}/>
-                <p style={{ marginTop: "-100px", color: "white", fontSize: "40px", fontWeight: 'bold' }}>LIVING</p>
+      <div className="carousel-image">
+        <img
+          src={category4}
+          alt="Category 4"
+          style={{
+            width: "1200px",
+            height: "600px",
+            objectFit: "cover",
+            marginTop: "50px",
+            marginLeft: "351.5px",
+          }}
+          onClick={() => handleImageClick("Living")}
+        />
+        <p
+          style={{
+            marginTop: "-100px",
+            color: "white",
+            fontSize: "40px",
+            fontWeight: "bold",
+          }}
+        >
+          LIVING
+        </p>
       </div>
     </Slider>
   );
