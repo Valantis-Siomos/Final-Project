409f37d49a3f6be843c322c9d027427047afe24b
Valantis <siopis2008@hotmail.com>
2023-11-25 22:31:11 +0200

    changes home-cart-productsByCategory footer remake

--- client/src/components/cart.js
+++ client/src/components/cart.js
@@ -42,7 +42,6 @@ const Cart = () => {
 
   return (
     <div className="mainDivCart">
-      
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
@@ -50,18 +49,25 @@ const Cart = () => {
           {cartItems.map((item, index) => (
             <div key={`${item.id}_${index}`} className="item">
               <p>{item.title}</p>
-              {/* <p>{item.description}</p> */}
               <p>{item.price}</p>
-              <button onClick={() => deleteItem(index)}>Delete</button>
-              
+              <button className="deleteButton" onClick={() => deleteItem(index)}>
+                <span className="text">Delete</span>
+                <span className="icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                  </svg>
+                </span>
+              </button>
             </div>
           ))}
-          <p className="total">Total: {calculateTotalPrice()} €</p>
+          <div className="totalClear">
+            <p className="total">Total: {calculateTotalPrice()} €</p>
+            <button className="clearCartButton" onClick={clearCart}>
+              Clear Cart
+            </button>
+          </div>
           <GooglePay />
-          <button className="clearCartButton" onClick={clearCart}>Clear Cart</button>
-          
         </div>
-        
       )}
     </div>
   );
--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,25 +1,44 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import "./footer.css"
+import "./footer.css";
 import logo from "../assets/logo.png";
-
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaYoutubeSquare } from "react-icons/fa";
 
 function Footer() {
-  
-
   return (
     <>
       <footer className="footer">
-      <img className="imgNav" src={logo} alt="Logo" />
-      <div>
-        <p>La Casa &copy; {new Date().getFullYear()} All Rights Reserved</p>
-      </div>
-      <div>
-        <h3>linkedin</h3>
-        <h3>github</h3>
-      </div>
-    </footer>
-      
+        <div className="footerMain">
+          <div>
+            <img className="imgNav" src={logo} alt="Logo" />
+            <p className="textFooter">
+              Embark on a timeless journey in the world of exquisite
+              furnishings, where the dream began on September 15, 1523. At our
+              furniture haven, quality is not just a promise; it's our legacy.
+            </p>
+            <p className="copyrights">
+              La Casa &copy; {new Date().getFullYear()} | ALL RIGHTS RESERVED{" "}
+            </p>
+          </div>
+
+          <div className="contact">
+            <h2 className="cont" >Contct Us</h2>
+            <div className="icons">
+              <FaGithubSquare className="github" />
+              <FaLinkedin className="linkedln" />
+              <FaFacebookSquare className="facebook" />
+              <FaSquareInstagram className="insta" />
+              <FaSquareXTwitter className="twitter" />
+              <FaYoutubeSquare className="youtube" />
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
