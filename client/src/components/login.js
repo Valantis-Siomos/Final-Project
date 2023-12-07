df9af01711293eb54087a45b82958904cbcd76e1
Valantis <siopis2008@hotmail.com>
2023-12-07 20:43:36 +0200

    modal Login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
 import "./login.css";
 
 function Login() {
@@ -18,20 +20,35 @@ function Login() {
         password,
       });
       if (res.status === 200) {
-        alert(res.data.msg);
+        // alert(res.data.msg);
+        Swal.fire({
+          title: "Welcome",
+          text: email,
+          customClass: {
+            popup: "custom-popup-class",
+            confirmButton: "btn-ok",
+          },
+        });
         localStorage.setItem("token", res.data.token);
         navigate("/");
-        
       }
     } catch (err) {
-      alert("Log in failed, check your email or password.");
+      // alert("Log in failed, check your email or password.");
+      Swal.fire({
+        title: "Log in failed, check your email or password",
+        // text: "You clicked the button!",
+        icon: "error",
+        customClass: {
+          popup: "custom-popup-class",
+          confirmButton: "btn-ok-fail",
+        },
+      });
     }
   }
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-
 
   return (
     <div className="outerDiv">
@@ -41,7 +58,8 @@ function Login() {
           {/* <h1>Login</h1> */}
           <form className="loginForm" onSubmit={handleLogin}>
             <label htmlFor="email">Email</label>
-            <input className="email"
+            <input
+              className="email"
               id="email"
               type="email"
               value={email}
@@ -50,21 +68,24 @@ function Login() {
             />
             <label htmlFor="pass">Password</label>
             <div className="password-input-container">
-            <input className="password"
-              id="pass"
-              type={showPassword ? "text" : "password"}
-              placeholder=""
-              size="40"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {showPassword ? (
+              <input
+                className="password"
+                id="pass"
+                type={showPassword ? "text" : "password"}
+                placeholder=""
+                size="40"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {showPassword ? (
                 <FaEyeSlash onClick={togglePasswordVisibility} />
               ) : (
                 <FaEye onClick={togglePasswordVisibility} />
               )}
-              </div>
-            <button className="button" type="submit">Sign in</button>
+            </div>
+            <button className="button" type="submit">
+              Sign in
+            </button>
           </form>
         </div>
       </div>
